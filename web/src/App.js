0f9546bd1c384fd4cce46c7a9dead8f1146af88b
vincenttommi <vincenttommi@gmail.com>
2023-04-25 22:27:31 +0300

    adding form

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import TransactionList from './components/TransactionList';
+import TransactionTable from './components/TransactionTable'
 
 function App() {
   return (
     <div>
       <h1>Mzee Budget</h1>
       <TransactionList />
+      <TransactionTable/>
+      <addform/>
     </div>
   );
 }
---
+++ web/src/components/addform.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react"
+import React,{useState} from React
+const addform = () => {
+
+const category = ["Food","Income","Fashion","Gift","Transportation",]
+
+
+
+  return (
+
+  )
+}
+
+export default addform
